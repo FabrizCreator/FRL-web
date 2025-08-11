a7dcb0baa61c2a382ed6c7989a3c3c9fda19a5db
FabrizCreator <ercole.fabrizio@outlook.it>
2025-08-11 20:24:50 +0200

    Create Admin.tsx

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,6 @@
+export default function Admin(){
+  return <div style={{color:'#fff',padding:24}}>
+    <h2>Admin</h2>
+    <p>Qui aggiungeremo: Leghe, Competitions (Serie A 2025/2026), Regole, Import, Branding.</p>
+  </div>
+}
