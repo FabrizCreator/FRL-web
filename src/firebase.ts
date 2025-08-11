6ddce42b5fc1a45e373c5884e5f7d045f5c31454
FabrizCreator <ercole.fabrizio@outlook.it>
2025-08-11 22:19:04 +0200

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// ⚠️ COMPILA QUESTI VALORI DAL PROGETTO FIREBASE (app web)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "REPLACE",
-  authDomain: "REPLACE",
-  projectId: "REPLACE",
-  storageBucket: "REPLACE",
-  messagingSenderId: "REPLACE",
-  appId: "REPLACE"
+  apiKey: "AIzaSyAChm1BuS7bXfahVzdmcxD17_1qn2XXB50",
+  authDomain: "fantaroccolegend.firebaseapp.com",
+  projectId: "fantaroccolegend",
+  storageBucket: "fantaroccolegend.firebasestorage.app",
+  messagingSenderId: "554203783674",
+  appId: "1:554203783674:web:3255358df177cef7608443"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
